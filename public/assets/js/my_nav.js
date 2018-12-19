cf7cd729accfcaeaeba0c6fcc2b3e601625303ea
Shaurya Bhatnagar <shauryabhatnagar@Shauryas-MacBook-Pro.local>
2018-12-20 01:20:49 +0530

    new navbar

---
+++ public/assets/js/my_nav.js
@@ -0,0 +1,39 @@
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = jQuery('nav').outerHeight();
+
+jQuery(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = jQuery(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        jQuery('nav').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + jQuery(window).height() < jQuery(document).height()) {
+            jQuery('nav').removeClass('nav-up').addClass('nav-down nav-color');
+        }
+    }
+    
+    lastScrollTop = st;
+}
