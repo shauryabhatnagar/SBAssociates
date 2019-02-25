7da440ae4008d7c84660ab118a428b7a9e777a12
Shaurya Bhatnagar <shauryabhatnagar@Shauryas-MacBook-Pro.local>
2019-02-25 18:43:37 +0530

    image compression

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -8,14 +8,7 @@ $(document).ready(function ($) {
         }
     };
 
-    function init() {
-        var imgDefer = document.getElementsByTagName('img');
-        for (var i = 0; i < imgDefer.length; i++) {
-            if (imgDefer[i].getAttribute('data-src')) {
-                imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
-            }
-        }
-    }
+    $("img.lazy").lazyload();
     
     "use strict";
 
