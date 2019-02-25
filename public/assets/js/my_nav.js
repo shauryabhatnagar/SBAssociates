8a0aff3c40d79c8f9a02a452404a6f05e208cf95
Shaurya Bhatnagar <shauryabhatnagar@Shauryas-MacBook-Pro.local>
2019-02-25 18:03:54 +0530

    image compression

--- public/assets/js/my_nav.js
+++ public/assets/js/my_nav.js
@@ -1,15 +1,14 @@
-
 // Hide Header on on scroll down
 var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
 var navbarHeight = jQuery('nav').outerHeight();
 
-jQuery(window).scroll(function(event){
+jQuery(window).scroll(function (event) {
     didScroll = true;
 });
 
-setInterval(function() {
+setInterval(function () {
     if (didScroll) {
         hasScrolled();
         didScroll = false;
@@ -18,22 +17,22 @@ setInterval(function() {
 
 function hasScrolled() {
     var st = jQuery(this).scrollTop();
-    
+
     // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
+    if (Math.abs(lastScrollTop - st) <= delta)
         return;
-    
+
     // If they scrolled down and are past the navbar, add class .nav-up.
     // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
+    if (st > lastScrollTop && st > navbarHeight) {
         // Scroll Down
         jQuery('nav').removeClass('nav-down').addClass('nav-up');
     } else {
         // Scroll Up
-        if(st + jQuery(window).height() < jQuery(document).height()) {
+        if (st + jQuery(window).height() < jQuery(document).height()) {
             jQuery('nav').removeClass('nav-up').addClass('nav-down nav-color');
         }
     }
-    
+
     lastScrollTop = st;
 }
