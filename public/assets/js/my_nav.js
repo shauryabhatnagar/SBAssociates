ad8de5704214bf0694b14e2365b28645f823d41d
Shaurya Bhatnagar <shauryabhatnagar@Shauryas-MacBook-Pro.local>
2019-03-21 12:22:08 +0530

    remove nav background at the top

--- public/assets/js/my_nav.js
+++ public/assets/js/my_nav.js
@@ -26,21 +26,27 @@ function hasScrolled() {
     // This is necessary so you never see what is "behind" the navbar.
     if (st > lastScrollTop && st > navbarHeight) {
         // Scroll Down
-        jQuery('nav').removeClass('nav-down').addClass('nav-up');
+        jQuery('nav').removeClass('nav-down nav-color').addClass('nav-up');
     } else {
         // Scroll Up
         if (st + jQuery(window).height() < jQuery(document).height() && st != 0) {
             jQuery('nav').removeClass('nav-up').addClass('nav-down nav-color');
         }
     }
+    //Always remove nav background at the the top 
+    if(st == 0){
+       jQuery('nav').removeClass('nav-down nav-color');
+    }
 
     lastScrollTop = st;
 }
 
 function init() {
-var imgDefer = document.getElementsByClassName('img1');
-for (var i=0; i<imgDefer.length; i++) {
-if(imgDefer[i].getAttribute('data-src')) {
-imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
-} } }
+    var imgDefer = document.getElementsByClassName('img1');
+    for (var i=0; i<imgDefer.length; i++) {
+        if(imgDefer[i].getAttribute('data-src')) {
+            imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+        } 
+    } 
+}
 window.onload = init;
