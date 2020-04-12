0c277c1f659fc73960b5e0acbdee44644ef34b1c
shauryabhatnagar <32093382+shauryabhatnagar@users.noreply.github.com>
2020-04-12 22:14:14 +0530

    Update main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function ($) {
 
+    $('.gsc-control-cse').css({"border-color":"#FFFFFF00","background-color":"transparent"});	
+    
     function loadStyleSheet(src) {
         if (document.createStyleSheet) {
             document.createStyleSheet(src);
